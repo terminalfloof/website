f0a6c45a4680ea139996e73b9cc87273383c9588
Ubuntu <ubuntu@ip-172-31-85-249.ec2.internal>
2025-03-17 03:37:24 +0000

    update adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import fs from 'fs';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		https: {
+			key: fs.readFileSync('/etc/ssl/certs/floof.key'),
+			cert: fs.readFileSync('/etc/ssl/certs/terminalfloof_me.crt')
+		}
+	}
 });
