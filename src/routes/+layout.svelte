f4dc9a93fafbbcfea99bca62d2004b544194a2b0
floof <rglyxr@gmail.com>
2025-04-13 06:09:38 -0500

    i am speed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import { injectSpeedInsights } from '@vercel/speed-insights';
 	import '../app.css';
 	let { children } = $props();
+
+	injectSpeedInsights();
 </script>
 
 {@render children()}
